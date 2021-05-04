0255f2057461e8e7fd02cfbe03249ab73407ad00
Stanimir Iglev <iglevv@gmail.com>
2021-05-04 22:27:52 +0200

    Add basic structure of top champions section

--- src/SummonerInfo.js
+++ src/SummonerInfo.js
@@ -23,7 +23,10 @@ class SummonerInfo extends React.Component {
       <div className="SummonerInfo">
         <SummonerData summoner={summoner} />
         <MatchHistory summoner={this.props.summoner} />
-        <BestChampions summoner={this.props.summoner} />
+        <BestChampions
+          summoner={this.props.summoner}
+          champions={this.props.champions}
+        />
       </div>
     );
   }
@@ -70,12 +73,57 @@ function MatchHistory(props) {
 }
 
 function BestChampions(props) {
+  const topTenChamps = props.summoner.mastery.slice(0, 10);
+  const champions = props.champions;
+  let bestChamps = {};
+  for (let champ of topTenChamps) {
+    bestChamps[champ.championId] = {
+      name: "",
+      championPoints: champ.championPoints,
+      championLevel: champ.championLevel,
+      chestGranted: champ.chestGranted,
+    };
+  }
+  for (let champId in champions) {
+    if (Object.keys(bestChamps).includes(champions[champId].key)) {
+      bestChamps[champions[champId].key].name = champions[champId].id;
+    }
+  }
+  console.log("bestchamps: ", bestChamps);
   return (
     <div className="bestChampions">
       <center>
         <h2>Top Champions</h2>
+        <ChampionEntries champions={bestChamps} />
       </center>
     </div>
+  );
+}
+
+function ChampionEntries(props) {
+  let champs = [];
+  for (let champId in props.champions) {
+    champs.push(
+      <tr key={champId}>
+        <td>{props.champions[champId].name}</td>
+        <td>{props.champions[champId].championPoints}</td>
+        <td>{props.champions[champId].championLevel}</td>
+        <td>{props.champions[champId].chestGranted ? "Yes" : "No"}</td>
+      </tr>
+    );
+  }
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Champion</th>
+          <th>Points</th>
+          <th>Level</th>
+          <th>Chest granted</th>
+        </tr>
+      </thead>
+      <tbody>{champs}</tbody>
+    </table>
   );
 }
 export default SummonerInfo;
